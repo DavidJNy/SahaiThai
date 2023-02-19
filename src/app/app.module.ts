6c211b3131871dd605404e9dae84589a8e6a20c6
David <davidjohnsonny@gmail.com>
2023-02-18 18:52:06 -0800

    Removed xyz component; added 3x components (Home, Menu, & Contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    NavbarComponent,
+    HomeComponent,
+    MenuComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
